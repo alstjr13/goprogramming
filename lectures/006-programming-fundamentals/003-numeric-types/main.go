6429915cd677f9d52815e2470669c75b8bc347b7
alstjr13 <alstjr6030@naver.com>
2023-04-06 20:37:01 +0900

    you're doing great

--- lectures/006-programming-fundamentals/003-numeric-types/main.go
+++ lectures/006-programming-fundamentals/003-numeric-types/main.go
@@ -1,18 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 var x int
 var y float64
 
+var z int8
+
+var a byte //same as uint8
+var b rune //same as int32
+
 func main() {
 	x = 42
 	y = 42.34534
+	z = -127
 
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Printf("%T\n", x)
 	fmt.Printf("%T\n", y)
+
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+
+	fmt.Println(runtime.GOOS)   // windows
+	fmt.Println(runtime.GOARCH) // amd64
 
 }
 
